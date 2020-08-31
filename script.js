135812e39441ab3ec10f85566be3ad04afbc2b23
alexmalinowski <70277551+alexmalinowski@users.noreply.github.com>
2020-08-31 11:52:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 document.body.querySelector("#cool").innerHTML="Hello There"
 var prompter = prompt("Hello! What is your name?");
 document.body.querySelector("#lessCool").innerHTML=prompter;
-
-var prompter = prompt("What is your favorite color?");
