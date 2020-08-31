cadbb60be769a2b75d5730aa7bb9e0099b16f2e9
alexmalinowski <70277551+alexmalinowski@users.noreply.github.com>
2020-08-31 11:33:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.body.querySelector("#cool").innerHTML="Hello There"
+var prompter = prompt("Hello! What is your name?");
+document.body.querySelector("#lessCool").innerHTML=prompter;
+
+var prompter = prompt("What is your favorite color?");
